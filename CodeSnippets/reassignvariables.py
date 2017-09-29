20406209dd3012ac5f384c018c07a713ba1fc54d
KernelForbin <kf@thephish.fm>
2017-09-28 20:12:12 -0400

    Update and rename lab1problem3.py to reassignvariables.py

---
+++ CodeSnippets/reassignvariables.py
@@ -0,0 +1,11 @@
+__author__ = 'KernelForbin'
+# Reassign values via variables
+x = input("Please input a value: ")
+y = input("Please input anohter value: ")
+print("x has the value: " + x)
+print("y has the value: " + y)
+z = x
+x = y
+y = z
+print("x has the value: " + x)
+print("y has the value: " + y)
