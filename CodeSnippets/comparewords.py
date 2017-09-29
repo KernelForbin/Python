4cfe89aa21a7494711028d9a45a4e715ecda3950
KernelForbin <kf@thephish.fm>
2017-09-28 20:11:06 -0400

    Update and rename lab1problem2.py to comparewords.py

---
+++ CodeSnippets/comparewords.py
@@ -0,0 +1,7 @@
+l__author__ = 'KernelForbin'
+# Comparing if two words are the same of different
+word1 = input("Please input your first word: ")
+word2 = input("Please input your second word: ")
+if word1 == word2:
+    print("These words are the same.")
+else: print("These words are not the same.")
