2ae7fcac7d1f6109635f85385e33ec2841d5383e
KernelForbin <kf@thephish.fm>
2017-10-05 20:00:03 -0400

    Update NumpyAndMatplotlib.py

--- CodeSnippets/NumpyAndMatplotlib.py
+++ CodeSnippets/NumpyAndMatplotlib.py
@@ -1,24 +1,10 @@
-
-# coding: utf-8
-
-# In[1]:
-
-
 import numpy as np
 import matplotlib.pyplot as plt
-
-
-# In[3]:
-
 
 # Computer x and y coordinates for points on a sine curve
 x = np.arange(0, 4 * np.pi, 0.05)
 y = np.sin(x)
 z = np.sin(x**2)
-
-
-# In[4]:
-
 
 #Plot the points using matplotlib
 plt.plot(x, y)
